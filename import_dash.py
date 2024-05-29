28374c6f8ea2f28ad51f175cfba4f1a3ddcce440
Andres Arevalo Fajardo <a.arevalof@uniandes.edu.co>
2024-05-29 11:50:00 -0500

    host configuration

--- import_dash.py
+++ import_dash.py
@@ -297,4 +297,4 @@ def update_output(contents, filename):
 # Ejecutar la aplicación de Dash
 
 if __name__ == '__main__':
-  app.run_server(debug=True)
+  app.run_server(host = "0.0.0.0", debug=True, port=8050)
